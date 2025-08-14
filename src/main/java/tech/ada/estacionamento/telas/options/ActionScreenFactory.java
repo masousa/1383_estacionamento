1def7e76d6bb2e35ad9361c524da6821979ff879
Matheus Sousa <alvessousam6@gmail.com>
2025-08-14 19:16:57 -0300

    Alterando a forma de chamar os métodos utilizando programação funcional.

--- src/main/java/tech/ada/estacionamento/telas/options/ActionScreenFactory.java
+++ src/main/java/tech/ada/estacionamento/telas/options/ActionScreenFactory.java
@@ -1,5 +1,6 @@
 package tech.ada.estacionamento.telas.options;
 
+import tech.ada.estacionamento.dominio.EntradaSaida;
 import tech.ada.estacionamento.services.ClienteService;
 import tech.ada.estacionamento.services.EntradaSaidaService;
 import tech.ada.estacionamento.services.VeiculoService;
@@ -7,7 +8,9 @@ import tech.ada.estacionamento.telas.GetVeiculoScreen;
 import tech.ada.estacionamento.telas.OpcaoTela;
 import tech.ada.estacionamento.telas.GetProprietarioScreen;
 
+import java.time.LocalDateTime;
 import java.util.Scanner;
+import java.util.function.Consumer;
 
 public class ActionScreenFactory {
 
@@ -24,16 +27,27 @@ public class ActionScreenFactory {
 
 	public void getCorrectScreen(OpcaoTela opcaoTela){
 		ScreenAction screenAction = null;
+
 		switch (opcaoTela){
 			case CADASTRAR_CLIENTE -> screenAction = new ClienteScreenAction(clienteService, scanner);
 			case CADASTRAR_VEICULO -> screenAction =  new VeiculoScreenAction(scanner, veiculoService,
 					new GetProprietarioScreen(clienteService, scanner).getProprietario());
 			case REGISTRAR_ENTRADA -> screenAction = new EntradaScreenAction(scanner,
 					new GetVeiculoScreen(scanner,veiculoService).getVeiculo(), entradaSaidaService);
-			case REGISTRAR_SAIDA -> screenAction = new SaidaScreenAction(scanner, entradaSaidaService);
 
-			default -> screenAction = new ExitScreenAction();
+			case REGISTRAR_SAIDA -> consumer= this::registrarSaida;
+
+			default -> consumer = sc -> System.out.println("Obrigado por utilizar nossos sistemas");
 		}
+
 		screenAction.execute();
+	}
+
+	private void registrarSaida(Scanner scanner) {
+		System.out.println("Informar a placa do  veiculo");
+		String placaVeiculo = scanner.next();
+
+		EntradaSaida saida = entradaSaidaService.findByPlaca(placaVeiculo);
+		saida.setDataHoraSaida(LocalDateTime.now());
 	}
 }
