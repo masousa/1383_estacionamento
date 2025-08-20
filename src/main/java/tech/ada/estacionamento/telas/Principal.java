c7f831103c16669b0dcc1521b711592af7615ebb
masousa <matheus.louko@gmail.com>
2025-08-19 21:46:18 -0300

    Adicionando a funcionalidade de salvar o cliente. (#3)
    Co-authored-by: Matheus Sousa <alvessousam6@gmail.com>

--- src/main/java/tech/ada/estacionamento/telas/Principal.java
+++ src/main/java/tech/ada/estacionamento/telas/Principal.java
@@ -2,18 +2,28 @@ package tech.ada.estacionamento.telas;
 
 import tech.ada.estacionamento.telas.options.ActionScreenFactory;
 
+import java.io.FileNotFoundException;
+import java.nio.file.FileSystemNotFoundException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Principal {
 	public static void main(String[] args) {
 
-		Scanner leitorEntrada = new Scanner(System.in);
-		var actionScreenFactory = new ActionScreenFactory(leitorEntrada);
-		int option = 0;
-		do{
-			option = imprimirOpcoes(leitorEntrada,actionScreenFactory);
+		//Scanner leitorEntrada = new Scanner(System.in);
+		//try {
+			Scanner leitorEntrada = new Scanner(System.in);
+			//Scanner leitorEntrada = new Scanner(getInput().toFile());
+			var actionScreenFactory = new ActionScreenFactory(leitorEntrada);
+			int option = 0;
+			do {
+				option = imprimirOpcoes(leitorEntrada, actionScreenFactory);
 
-		}while(option!=OpcaoTela.SAIR.getOpcao());
+			} while (option != OpcaoTela.SAIR.getOpcao());
+		/*} catch (FileNotFoundException fne) {
+			throw new RuntimeException(fne);
+		}*/
 
 	}
 
@@ -30,4 +40,9 @@ public class Principal {
 		return option;
 
 	}
+
+	private static Path getInput(){
+		return Paths.get("src", "main", "resources","input.entry").toAbsolutePath();
+	}
+
 }
